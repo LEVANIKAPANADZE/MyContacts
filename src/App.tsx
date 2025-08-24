8306a70cf13be59a8a417e1aa7916cb0b18a54bb
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2025-08-24 16:52:52 +0400

    Imported icons and now making data.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <></>;
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-gray-900 via-gray-800 to-black text-white">
+      <header>
+        <h1>Levani Kapanadze</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
