b769a55f1780ebccbbb4fdd653729604617782ab
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2025-11-07 23:58:56 +0400

    Woeking on styles

--- src/App.tsx
+++ src/App.tsx
@@ -106,7 +106,10 @@ export default function App() {
                 <h3 className="text-xl sm:text-2xl font-semibold text-center bg-gradient-to-r from-white via-gray-200 to-white bg-clip-text text-transparent">
                   {element.mediaTitle}
                 </h3>
-                <p className="text-xs text-white/70 text-center max-w-[260px]">
+                <p
+                  className="text-xs text-white/70 
+                text-center max-w-[260px]"
+                >
                   {element.description || element.mediaTitle}
                 </p>
                 <div className="w-full flex items-center gap-3">
